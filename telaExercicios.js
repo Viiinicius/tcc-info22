3d96bac166e796f4cbeda648ef8894c1265b9ec3
EduardodaSilva308 <160235401+EduardodaSilva308@users.noreply.github.com>
2024-03-17 19:59:04 -0300

    hh

---
+++ telaExercicios.js
@@ -0,0 +1,6 @@
+function add(){
+    document.getSelection("novoexercicio").innerHTML = " <ul><li>
+    <input id="nomeexercicio" name="nomeexercicio" required="required" type="text"
+         placeholder="Nome do seu Exercício" />
+          </li></ul> ";
+}
