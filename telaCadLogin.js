2521aef1d5c0e9a7c097c47ef5b147bb8722c71d
Viiinicius <134502197+Viiinicius@users.noreply.github.com>
2024-03-19 09:14:55 -0300

    ALTERAÇÃO JS

---
+++ telaCadLogin.js
@@ -0,0 +1,14 @@
+const loginInput = document.querySelector("#login")
+const emailInput = document.querySelector("#email_login")
+const passwordInput = document.querySelector("#senha_login")
+
+console.log(emailInput, passwordInput, loginInput)
+
+loginInput.addEventListener("submit",(event)=> {
+
+    event.preventDefault()
+
+    // verifica se o nome esta vazio
+    if(emailInput.value ===)
+
+})
--- telaExercicios.js
+++
@@ -1,6 +0,0 @@
-function add(){
-    document.getSelection("novoexercicio").innerHTML = " <ul><li>
-    <input id="nomeexercicio" name="nomeexercicio" required="required" type="text"
-         placeholder="Nome do seu Exercício" />
-          </li></ul> ";
-}
